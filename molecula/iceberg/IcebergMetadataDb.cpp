7285ade49a8afcb3ce50e91069f979175becf54f
Igor Demura <igor.demura@gmail.com>
2026-01-25 05:54:23 -0800

    Add Iceberg metadata DB

---
+++ molecula/iceberg/IcebergMetadataDb.cpp
@@ -0,0 +1,11 @@
+#include "molecula/iceberg/IcebergMetadataDb.hpp"
+
+#include <glog/logging.h>
+
+#include <stdexcept>
+
+namespace molecula::iceberg {
+
+// TODO
+
+} // namespace molecula::iceberg
---
+++ molecula/iceberg/IcebergMetadataDb.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <memory>
+#include <string>
+#include <string_view>
+#include <vector>
+
+namespace molecula::iceberg {
+
+class MetadataDb {
+public:
+    MetadataDb() = default;
+};
+
+} // namespace molecula::iceberg
